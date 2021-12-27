dbe0f0d3aaa881a3b6da0ac22ba7d74462e90795
lichunrui <1240873204@qq.com>
2021-12-27 14:50:58 +0800

    修改hello

--- src/main/java/com/test/Hello.java
+++ src/main/java/com/test/Hello.java
@@ -3,5 +3,9 @@ package com.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git ");
+        System.out.println("hello git2 ");
+        System.out.println("hello git 3");
+        System.out.println("hello git4 ");
+        System.out.println("hello git5 ");
     }
 }
